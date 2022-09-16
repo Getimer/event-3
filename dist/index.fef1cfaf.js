ad80a168de0fbbcb00d8b7cd6876f67d33e3eb81
nmer <2309382194@qq.com>
2022-09-16 19:22:30 +0800

    2.0

--- dist/index.fef1cfaf.js
+++ dist/index.fef1cfaf.js
@@ -456,15 +456,18 @@ function hmrAcceptRun(bundle, id) {
 
 },{}]},["7mHCm"], null, "parcelRequire94c2")
 const div1 = document.querySelector("#div1");
+setTimeout(()=>{
+    const button = document.createElement("button");
+    button.textContent = "click1";
+    button.dataset.id = 1;
+    div1.appendChild(button);
+}, 1000);
 div1.addEventListener("click", (e)=>{
     const t = e.target;
     if (t.tagName.toLowerCase() === "button") {
         console.log("button被点击了");
-        console.log("被点击的dataset-id是:" + t.dataset.id);
-    }
-    if (t.tagName.toLowerCase() === "span") {
-        console.log("span被点击了");
-        console.log("被点击的span是:" + t.textContent);
+        console.log("被点击的button是:" + t.textContent);
+        console.log("被点击的button dataset-id是:" + t.dataset.id);
     }
 });
 
