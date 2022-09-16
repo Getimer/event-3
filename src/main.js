c25bbef0e7f305da1cd76dee8fd5b3f5f5005e62
nmer <2309382194@qq.com>
2022-09-16 22:31:10 +0800

    4.0

--- src/main.js
+++ src/main.js
@@ -1,21 +1,28 @@
 setTimeout(() => {
   const button = document.createElement("button");
-  button.textContent = "click1";
-  button.dataset.id = 1;
+  const span = document.createElement("span");
+  span.textContent = "click1";
   div1.appendChild(button);
+  button.appendChild(span);
 }, 1000);
 on("click", "#div1", "button", () => {
   console.log("button被点击了");
 });
 function on(eventType, element, selector, fn) {
-  if (!element instanceof Element) {
+  if (!(element instanceof Element)) {
     element = document.querySelector(element);
   }
 
   element.addEventListener(eventType, (e) => {
-    const t = e.target;
-    if (t.matches(selector)) {
-      fn(e);
+    let t = e.target;
+    while (!t.matches(selector)) {
+      if (element === t) {
+        t = null;
+        break;
+      }
+      t = t.parentNode;
     }
+    t && fn.call(t, e, t);
   });
+  return element;
 }
